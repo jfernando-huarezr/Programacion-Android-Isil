c4ee93dffc83c694363c6d43552506f89f068fd8
Jose Fernando Huarez Reyes <jose.huarezr@gmail.com>
2023-09-22 11:35:59 -0500

    semana 4

--- app/src/main/java/com/huarezreyes/primeraapp/EmpleadosActivity.kt
+++ app/src/main/java/com/huarezreyes/primeraapp/EmpleadosActivity.kt
@@ -1,46 +1,34 @@
 package com.huarezreyes.primeraapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.huarezreyes.primeraapp.ui.theme.PrimeraAppTheme
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class EmpleadosActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            PrimeraAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting2("Android")
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        leerServicio()
+    } /*onCreate*/
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview2() {
-    PrimeraAppTheme {
-        Greeting2("Android")
-    }
+    private fun leerServicio() {
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://servicios.campus.pe/proveedores.php"
+
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            { response ->
+                Log.d("DATOS", response)
+                //llenarLista(response)
+            },
+            {
+                Log.d("DATOSERROR", it.message.toString())
+            })
+
+        queue.add(stringRequest)
+    } /*leerServicio*/
 }
