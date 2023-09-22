18f7c335d3db5053549fb50b27098d950ed25491
Jose Fernando Huarez Reyes <jose.huarezr@gmail.com>
2023-09-22 14:12:46 -0500

    funcion seleccionarCategoria TiendaActivity

--- app/src/main/java/com/huarezreyes/primeraapp/TiendaActivity.kt
+++ app/src/main/java/com/huarezreyes/primeraapp/TiendaActivity.kt
@@ -1,23 +1,26 @@
 package com.huarezreyes.primeraapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
@@ -84,25 +87,35 @@ class TiendaActivity : ComponentActivity() {
 
                 //es como lazy grid
                 LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
                     content ={
                         items(
                             items = arrayList,
                             itemContent = {categoria ->
-                                Column (
-                                    modifier = Modifier.padding(all = dimensionResource(id = R.dimen.espacio2))
-                                        .border(width = 1.dp, color = Color.Gray, shape = RectangleShape)
-                                        .padding(all = dimensionResource(id = R.dimen.espacio))
+                                Card (
+                                    colors = CardDefaults.cardColors(
+                                        containerColor = Color(0,250,200, 255)
+                                    ),
+                                    modifier = Modifier
+                                        .padding(all = dimensionResource(id = R.dimen.espacio2))
                                         .fillMaxWidth()
-
+                                        .clickable {
+                                            seleccionarCategoria(categoria)
+                                        }
                                 ) {
-                                    Text(text = categoria["nombre"].toString(),
-                                        style = MaterialTheme.typography.titleLarge,
-                                        color = Color.Red
-                                    )
-                                    Text(text = categoria["descripcion"].toString(),
-                                        style = MaterialTheme.typography.titleMedium
-                                    )
-                                    Text(text = categoria["total"].toString())
+                                    Column (
+                                        modifier = Modifier.padding(all = dimensionResource(id = R.dimen.espacio))
+                                    ) {
+                                        Text(text = categoria["nombre"].toString(),
+                                            style = MaterialTheme.typography.titleLarge,
+                                            color = Color.Red
+                                        )
+                                        Text(text = categoria["descripcion"].toString(),
+                                            style = MaterialTheme.typography.titleMedium
+                                        )
+                                        Text(text = stringResource(id = R.string.cantidad_productos) + " "+ categoria["total"].toString())
+                                    }
+
                                 }
 
                             }
@@ -112,5 +125,21 @@ class TiendaActivity : ComponentActivity() {
             }
 
         }
+    }
+
+    private fun seleccionarCategoria(categoria: HashMap<String, String>) {
+        val idcategoria = categoria["idcategoria"]
+        val nombre = categoria["nombre"]
+        val descripcion = categoria["descripcion"]
+
+        Toast.makeText(this,nombre,Toast.LENGTH_SHORT).show()
+
+        val bundle = Bundle().apply {
+            putString("idcategoria", idcategoria)
+            putString("nombre", nombre)
+            putString("descripcion", descripcion)
+        }
+
+        startActivity(Intent(this@TiendaActivity, ProductosActivity::class.java))
     }
 }
